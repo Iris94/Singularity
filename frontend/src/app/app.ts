717104b724309782b2a7efeb8a9a09423cdc44c9
Iris94 <your-email@example.com>
2025-12-13 15:30:38 +0100

    Initial commit

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,37 @@
+import {
+  Component,
+  signal,
+  OnInit,
+} from '@angular/core';
+import {
+  RouterOutlet,
+  Router,
+  Event,
+  NavigationEnd,
+} from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App implements OnInit {
+  protected readonly title = signal('frontend');
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(
+      (event: Event) => {
+        if (event instanceof NavigationEnd) {
+          setTimeout(
+            () =>
+              window.HSStaticMethods.autoInit(),
+            100,
+          );
+        }
+      },
+    );
+  }
+}
